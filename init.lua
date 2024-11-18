acc16cdbd8f1e64366fe55c29c0aa773ce3695e2
Alex Lawson <aplawson7707@gmail.com>
2024-11-18 09:36:23 -0800

    Add clipboard provider

--- init.lua
+++ init.lua
@@ -111,9 +111,9 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+-- Schedule the setting after `UiEnter` because it can increase startup-time.
+-- Remove this option if you want your OS clipboard to remain independent.
+-- See `:help 'clipboard'`
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
